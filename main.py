f1a47fec663f34f644d615ba540d6a775006f997
bogdan <bbv1052@gmail.com>
2021-09-09 22:21:35 +0300

    added file data csv

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import re
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 URL = "https://www.topuniversities.com/where-to-study/north-america/united-states/ranked-top-100-us-universities"
+final_data = []
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, "html.parser")
+#print(soup)
+#for link in soup.find_all(string=re.compile("University")):
 
-print(soup)
+get_data = soup.find_all(string=re.compile("University"))
+#print(get_data)
+
+for link in get_data:
+    gen_list = []
+    gen_list.append(link)
+    final_data.append(gen_list)
+print(final_data)
+
+with open('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter = ',')
+    writer.writerow("")
+    for i in range(0, len(final_data)):
+        writer.writerow(final_data[i])
 
